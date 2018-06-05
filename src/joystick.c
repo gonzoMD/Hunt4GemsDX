3331b06d92fed7912b0bc7d792b80d66fe5cf1fc
Robert Naumann <gonzomdx@gmail.com>
2018-06-05 21:52:56 +0200

    [JOYSTICK] Fix the joystick code * I confused pra and prb. No, the A and B of the control registers is not the same as A and B of the controller ports. * indentation fixes

--- src/joystick.c
+++ src/joystick.c
@@ -3,27 +3,26 @@
 
 int getstate(int port)
 {
-	
 	int state = 0;
-	
+
 	switch (port)
 	{
-	case PORT_A:
-		if (!(CIA1.pra & JOY_UP)) state += JOY_UP;
-		if (!(CIA1.pra & JOY_DOWN)) state += JOY_DOWN;
-		if (!(CIA1.pra & JOY_LEFT)) state += JOY_LEFT;
-		if (!(CIA1.pra & JOY_RIGHT)) state += JOY_RIGHT;
-		if (!(CIA1.pra & JOY_FIRE)) state += JOY_FIRE;
-		return state;
+		case PORT_B:
+			if (!(CIA1.pra & JOY_UP)) state += JOY_UP;
+			if (!(CIA1.pra & JOY_DOWN)) state += JOY_DOWN;
+			if (!(CIA1.pra & JOY_LEFT)) state += JOY_LEFT;
+			if (!(CIA1.pra & JOY_RIGHT)) state += JOY_RIGHT;
+			if (!(CIA1.pra & JOY_FIRE)) state += JOY_FIRE;
+			return state;
 		break;
-	case PORT_B:
-		if (!(CIA1.prb & JOY_UP)) state += JOY_UP;
-		if (!(CIA1.prb & JOY_DOWN)) state += JOY_DOWN;
-		if (!(CIA1.prb & JOY_LEFT)) state += JOY_LEFT;
-		if (!(CIA1.prb & JOY_RIGHT)) state += JOY_RIGHT;
-		if (!(CIA1.prb & JOY_FIRE)) state += JOY_FIRE;
-	default:
-		return state;
+		case PORT_A:
+			if (!(CIA1.prb & JOY_UP)) state += JOY_UP;
+			if (!(CIA1.prb & JOY_DOWN)) state += JOY_DOWN;
+			if (!(CIA1.prb & JOY_LEFT)) state += JOY_LEFT;
+			if (!(CIA1.prb & JOY_RIGHT)) state += JOY_RIGHT;
+			if (!(CIA1.prb & JOY_FIRE)) state += JOY_FIRE;
+		default:
+			return state;
 		break;
 	};
 }
