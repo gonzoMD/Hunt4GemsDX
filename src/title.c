91f25df1538a66774f5ce6c9eee91a128fdb6f68
Robert Naumann <gonzomdx@gmail.com>
2018-06-06 20:53:57 +0200

    [TITLE] Now that the joystick code is fixed, use it correctly

--- src/title.c
+++ src/title.c
@@ -7,6 +7,6 @@ void showtitle()
 {
 	DRAW_Screen(SCREEN_title, COLOR_BLACK, COLOR_BLACK);
 	CIA1.ddra = 224;
-	while (getstate(PORT_A) != JOY_FIRE) {  }
+	while (getstate(PORT_B) != JOY_FIRE) {  }
 	CIA1.ddra = 255;
 }
