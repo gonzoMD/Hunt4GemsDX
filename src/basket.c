ab6dce779da8cc3c41b0340aa157b6509801049e
Robert Naumann <gonzomdx@gmail.com>
2020-07-02 16:02:28 +0200

    [BASKET] change some sprite parameters to fix regressions after adding the gems. Change some values to binary to make it better readable. Make the basket moving faster.

--- src/basket.c
+++ src/basket.c
@@ -17,9 +17,9 @@ void setupbasket()
     VIC.spr_color[BASKET_GUY] = COLOR_LIGHTRED;
     VIC.spr_pos[BASKET_GUY].y = 191;
 
-    VIC.spr_ena |= 0xF;
-    VIC.spr_hi_x &=240;
-    VIC.spr_exp_y |=0x8;
+    VIC.spr_ena |= 0b10000111;
+    VIC.spr_hi_x = 0b00000000;
+    VIC.spr_exp_y |= 0b10000000;
     x_pos = 150;
 }
 
@@ -30,40 +30,40 @@ void movebasket()
         case JOY_LEFT:
             if (x_pos>24)
             {
-                x_pos--;
+                x_pos-=3;
             }
         break;
         case JOY_RIGHT:
             if (x_pos<273)
             {
-                x_pos++;
+                x_pos+=3;
             }
         break;
     }
 
     if(x_pos>207)
     {
-        VIC.spr_hi_x |=4;
+        VIC.spr_hi_x |=0b00000100;
         if(x_pos>231)
         {
-            VIC.spr_hi_x |=14;
+            VIC.spr_hi_x |=0b10000010;
             if(x_pos>255)
             {
-                VIC.spr_hi_x |=15;
+                VIC.spr_hi_x |=0b00000001;
             }
             else
             {
-                VIC.spr_hi_x &=254;
+                VIC.spr_hi_x &=0b11111110;
             }
         }
         else
         {
-            VIC.spr_hi_x &=245;
+            VIC.spr_hi_x &=0b01111101;
         }
     }
     else
     {
-        VIC.spr_hi_x &=251;
+        VIC.spr_hi_x &=0b11111011;
     }
 
     for(i=BASKET_LEFT;i<=BASKET_RIGHT;i++)
